bcbb02636a1c0f9def499c2dfbd2bda4e16f958a
Parsa Parvizi <138492415+Parsa-Parvizi@users.noreply.github.com>
2025-12-17 22:16:22 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -170,15 +170,15 @@ if not DEBUG:
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
 
-LOG_DIR = BASE_DIR / "logs"
+# LOG_DIR = BASE_DIR / "logs"
 # Logging Configuration
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {'simple': {'format': '[{asctime}] {levelname} {name}: {message}', 'style': '{'}},
-    'handlers': {'console': {'class': 'logging.StreamHandler', 'formatter': 'simple'}},
-    'root': {'handlers': ['console'], 'level': 'INFO'},
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {'simple': {'format': '[{asctime}] {levelname} {name}: {message}', 'style': '{'}},
+#     'handlers': {'console': {'class': 'logging.StreamHandler', 'formatter': 'simple'}},
+#     'root': {'handlers': ['console'], 'level': 'INFO'},
+# }
 
 
 # Pagination
